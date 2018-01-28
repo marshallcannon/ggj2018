b08a943e6dc356f17cdc92705ea72dd36666a350
marshallcannon <marshcannon@gmail.com>
2018-01-27 19:09:41 -0700

    Fixed Wiggle's inheritance

--- monsters/wiggles.lua
+++ monsters/wiggles.lua
@@ -1,12 +1,12 @@
 local Monster = require 'monsters/monster'
 
 local Wiggles = Class{
-    __inclues = Monster
+    __includes = Monster
 }
 
 function Wiggles:init(x, y, direction)
 
-    Monster(x, y, images.wiggles, direction)
+    Monster.init(self, x, y, images.wiggles, direction)
 
 end
 
